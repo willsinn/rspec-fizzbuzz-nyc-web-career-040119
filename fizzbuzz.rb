7e5d84569d8d3ff51f2d3435f9dd16813d8fd19a
w.h.sinn <w.h.sinn@gmail.com>
2019-03-20 14:45:29 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,6 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-def fizzbuzz()
-  if 
+def fizzbuzz(num)
+  if num%3=0&&num=1 
+    return 
 end
