ee666eae306b6713207e5f8ab92f6bc59dbe7e4c
w.h.sinn <w.h.sinn@gmail.com>
2019-03-20 15:59:57 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,7 +2,7 @@
 # See README.md for instructions on how to do this
 def fizzbuzz(int)
   if int % 3 == 0 && int % 5 == 0
-    return "Fizzbuzz"
+    return "FizzBuzz"
   elsif int % 3 == 0 
     return "Fizz"
   elsif int % 5 == 0
